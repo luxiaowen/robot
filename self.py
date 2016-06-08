c1249c8a1a2776a31eed096a8d350d9a0f8d579f
luxiaowen <luxiaowen@maimiaotech.com>
2016-06-08 16:34:46 +0800

    hah

---
+++ self.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: luxiaowen
+@contact: luxiaowen@maimiaotech.com
+@date: 2016-06-08 16:34
+@version: 0.0.0
+@license: Copyright Maimiaotech.com
+@copyright: Copyright Maimiaotech.com
+
+"""
+
+
+print 'who is the dashen of Maimiaotech'
